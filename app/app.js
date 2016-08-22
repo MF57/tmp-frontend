8e97f5a3413d72aee54d7ed71bdf03ebbe8fef73
MF57 <p-bochenek@outlook.com>
2016-08-22 23:20:44 +0200

    ui router works

--- app/app.js
+++ app/app.js
@@ -1,14 +1,43 @@
-'use strict';
+(function() {
+    'use strict';
 
-// Declare app level module which depends on views, and components
-angular.module('myApp', [
-  'ngRoute',
-  'myApp.view1',
-  'myApp.view2',
-  'myApp.version'
-]).
-config(['$locationProvider', '$routeProvider', function($locationProvider, $routeProvider) {
-  $locationProvider.hashPrefix('!');
+    angular
+        .module('myApp', ['ui.router'])
+        .config(baseConfiguration);
 
-  $routeProvider.otherwise({redirectTo: '/view1'});
-}]);
+    baseConfiguration.$inject = ['$stateProvider', '$urlRouterProvider'];
+    function baseConfiguration($stateProvider, $urlRouterProvider) {
+        //
+        // For any unmatched url, redirect to /state1
+        $urlRouterProvider.otherwise("/state1");
+        //
+        // Now set up the states
+        $stateProvider
+            .state('state1', {
+                url: "/state1",
+                templateUrl: "partials/state1.html"
+            })
+            .state('state1.list', {
+                url: "/list",
+                templateUrl: "partials/state1.list.html",
+                controller: function($scope) {
+                    $scope.items = ["A", "List", "Of", "Items"];
+                }
+            })
+            .state('state2', {
+                url: "/state2",
+                templateUrl: "partials/state2.html"
+            })
+            .state('state2.list', {
+                url: "/list",
+                templateUrl: "partials/state2.list.html",
+                controller: function($scope) {
+                    $scope.things = ["A", "Set", "Of", "Things"];
+                }
+            });
+    }
+
+})();
+
+
+
