b64b500e66fc61155a71311ea77171227a86a505
Passarinho <passarinho4@gmail.com>
2016-08-24 21:16:26 +0200

    changes

--- app/app.js
+++ app/app.js
@@ -1,7 +1,7 @@
 (function () {
     'use strict';
 
-    angular
+    var mainApp = angular
         .module('myApp', ['ui.router'])
         .config(baseConfiguration);
 
@@ -30,6 +30,79 @@
             })
 
     }
+
+    mainApp.factory('TokenStorage', ['$window', function ($window) {
+        var storageKey = 'auth_token';
+        return {
+            store: function (token) {
+                return localStorage.setItem(storageKey, token);
+            },
+            retrieve: function () {
+                return localStorage.getItem(storageKey);
+            },
+            clear: function () {
+                return localStorage.removeItem(storageKey);
+            },
+            decode: function (token) {
+                if (token === null) {
+                    return null;
+                }
+                var base64Url = token.split('.')[1];
+                var base64 = base64Url.replace('-', '+').replace('_', '/');
+                return JSON.parse($window.atob(base64));
+            },
+            isAuthenticated: function() {
+                return !(localStorage.getItem(storageKey) === null);
+            }
+        };
+    }]);
+
+    mainApp.config(function ($httpProvider) {
+        /**
+         * FYI
+         * The custom "X-Requested-With" is a conventional header sent by browser clients,
+         * and it used to be the default in Angular but they took it out in 1.3.0.
+         * Spring Security responds to it by not sending a "WWW-Authenticate" header in a 401 response,
+         * and thus the browser will not pop up an authentication dialog (which is desirable in our app since
+         * we want to control the authentication).
+         * @type {string}
+         */
+        $httpProvider.defaults.headers.common["X-Requested-With"] = 'XMLHttpRequest';
+    });
+
+    mainApp.service('urls', function () {
+        var enrollment = "http://localhost:8093/";
+        var core = "http://tmp-core.tegess.com:8092/";
+        var stats = "http://tmp-stats.tegess.com:8094/";
+        var authlog = "http://localhost:8090/";
+        var api = "api/";
+        this.enrollmentApi = enrollment + api;
+        this.coreApi = core + api;
+        this.statsApi = stats + api;
+        this.authlogApi = authlog + api;
+        this.appId="5730dd4bc9e77c000189ad7a";
+    });
+
+
+    mainApp.factory('TokenAuthInterceptor', function ($q, TokenStorage) {
+        return {
+            request: function (config) {
+                var authToken = TokenStorage.retrieve();
+                if (authToken) {
+                    config.headers['X-AUTH-TOKEN'] = authToken;
+                }
+                return config;
+            },
+            responseError: function (error) {
+                if (error.status === 401 || error.status === 403) {
+                    TokenStorage.clear();
+                }
+                return $q.reject(error);
+            }
+        };
+    }).config(function ($httpProvider) {
+        $httpProvider.interceptors.push('TokenAuthInterceptor');
+    });
 
 })();
 
