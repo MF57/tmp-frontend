e8119cf26793c7ca1acb79eac039b16e6bf66c52
MF57 <p-bochenek@outlook.com>
2016-09-04 12:57:07 +0200

    tests initialized but not passing

---
+++ app/layouts/auth/navbar_test.js
@@ -0,0 +1,19 @@
+/**
+ * Created by mf57 on 04.09.2016.
+ */
+'use strict';
+
+describe('myApp.navbar module', function() {
+
+    beforeEach(module('myApp.navbar'));
+
+    describe('navbar controller', function(){
+
+        it('should ....', inject(function($controller) {
+            //spec body
+            var navbarCtrl = $controller('NavbarController');
+            expect(navbarCtrl).toBeDefined();
+        }));
+
+    });
+});
