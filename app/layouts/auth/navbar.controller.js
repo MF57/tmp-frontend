7cb76d493ffa88ad9fd8392bf3f3b719d2ec3133
Passarinho <passarinho4@gmail.com>
2016-09-27 00:11:41 +0200

    not finished

--- app/layouts/auth/navbar.controller.js
+++ app/layouts/auth/navbar.controller.js
@@ -4,12 +4,22 @@
         .module('myApp')
         .controller('NavbarController', NavbarController);
 
-    NavbarController.$inject = ['TokenStorage', 'LoginService'];
-    function NavbarController(TokenStorage, LoginService) {
+    NavbarController.$inject = ['TokenStorage', 'LoginService', 'ApiUrls'];
+    function NavbarController(TokenStorage, LoginService, ApiUrls) {
         var vm = this;
         
         vm.isAuthenticated = TokenStorage.isAuthenticated();
         vm.logoutFunction = logoutFunction;
+        vm.username = TokenStorage.decode(TokenStorage.retrieve()).username;
+        vm.userPictureUrl = ApiUrls.authlogApi + "applications/" + ApiUrls.appId + "/users/" + vm.username + "/photo/" + "0969d37a-facd-4a0d-84ca-50cbd33969e1";
+
+        $(document).ready(function () {
+            $('ul.nav > li').click(function (e) {
+                e.preventDefault();
+                $('ul.nav > li').removeClass('active');
+                $(this).addClass('active');
+            });
+        });
 
         function logoutFunction() {
           //  toastr.success("See you, " + TokenStorage.decode(TokenStorage.retrieve()).username);
