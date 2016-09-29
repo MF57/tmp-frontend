35b4ba884cddad4d437756466c5dd2c0166c191b
Passarinho <passarinho4@gmail.com>
2016-09-29 23:59:35 +0200

    add photo upload and change user photo

--- app/app.module.js
+++ app/app.module.js
@@ -3,6 +3,21 @@
 
     angular
         .module('myApp', ['AuthModule', 'NotificationModule', 'ui.router', 'ngDialog', 'ngAnimate'])
+        .directive('fileModel', ['$parse', function ($parse) {
+            return {
+                restrict: 'A',
+                link: function (scope, element, attrs) {
+                    var model = $parse(attrs.fileModel);
+                    var modelSetter = model.assign;
+
+                    element.bind('change', function () {
+                        scope.$apply(function () {
+                            modelSetter(scope, element[0].files[0]);
+                        });
+                    });
+                }
+            };
+        }]);
     
 })();
 
--- app/partials/auth/userprofile/userprofile.controller.js
+++ app/partials/auth/userprofile/userprofile.controller.js
@@ -7,14 +7,50 @@
         .module('myApp')
         .controller('UserProfileController', UserProfileController);
 
-    UserProfileController.$inject = ['$state', '$timeout', 'ngDialog', 'TokenStorage', 'UserService'];
-    function UserProfileController($state, $timeout, ngDialog, TokenStorage, UserService) {
+    UserProfileController.$inject = ['$http', 'ApiUrls', 'TokenStorage', 'UserService', '$scope'];
+    function UserProfileController($http, ApiUrls, TokenStorage, UserService, $scope) {
         var vm = this;
         
+        vm.picture = {};
         vm.refreshUserData = refreshUserData;
+        vm.changePicture = changePicture;
+        vm.uploadPicture = uploadPicture;
 
         refreshUserData();
 
+        $scope.$watch(function () {
+            return vm.picture;
+        }, uploadPicture);
+
+        function uploadPicture() {
+            var reader = new FileReader();
+            reader.onload = function (e) {
+                vm.user.pictureURL = e.target.result;
+                $scope.$apply();
+            };
+            if(vm.picture instanceof Blob){
+                reader.readAsDataURL(vm.picture);
+            }
+        }
+
+        function changePicture() {
+            var form = new FormData();
+            form.append("file", vm.picture);
+            console.log(vm.picture);
+            var username = TokenStorage.decode(TokenStorage.retrieve()).username;
+            $http.post(`${ApiUrls.authlogApi}applications/${ApiUrls.appId}/users/${username}/photo`,
+                form, {
+                    transformRequest: angular.identity,
+                    headers: {'Content-Type': undefined}
+                }).then(
+                function successCallback(result) {
+                    console.log("Picture changed")
+                },
+                function failureCallback(result) {
+                    console.log("Error during changing picture");
+                });
+        }
+        
         function refreshUserData() {
             UserService.getUser(TokenStorage.decode(TokenStorage.retrieve()).username)
                 .$promise.then(successCallback, failueCallback);
