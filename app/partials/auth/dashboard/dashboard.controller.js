4cc9ea7e4dffb15cbd31a8473b0e95ee2d932e90
MF57 <p-bochenek@outlook.com>
2016-10-17 20:01:29 +0200

    dashboard data mocked

--- app/partials/auth/dashboard/dashboard.controller.js
+++ app/partials/auth/dashboard/dashboard.controller.js
@@ -4,20 +4,26 @@
         .module('myApp')
         .controller('DashboardCtrl', DashboardController);
 
-    DashboardController.$inject = ['Dashboard'];
-    function DashboardController(Dashboard) {
+    DashboardController.$inject = ['Dashboard', '$state'];
+    function DashboardController(Dashboard, $state) {
         var vm = this;
         vm.ownerTournaments = [];
         vm.refereeTournaments = [];
         vm.participantTournaments = [];
+        vm.loadAll = loadAll;
+        vm.goToTournament = goToTournament;
 
 
-        vm.loadAll = function() {
+        function loadAll() {
             var data = Dashboard.loadAll();
             vm.ownerTournaments = data.ownerTournaments;
             vm.refereeTournaments = data.refereeTournaments;
             vm.participantTournaments = data.participantTournaments;
-        };
+        }
+
+        function goToTournament(tournament) {
+            alert(tournament.tournamentId)
+        }
 
         vm.loadAll();
     }
--- app/partials/auth/dashboard/dashboard.service.js
+++ app/partials/auth/dashboard/dashboard.service.js
@@ -17,15 +17,86 @@
 
         function loadAll() {
             var tournaments = {};
-            tournaments.ownerTournaments = ['Tournament 1', 'Tournament2', 'Tournament3', 'Tournament 4', 'Tournament5', 'Tournament6', 'Tournament7'];
+            tournaments.ownerTournaments = [
+                {
+                    tournamentId: "507c7f79bcf86cd7994f6c0e",
+                    name: "Tournament 1",
+                    beginDate: "2016-12-19 19:00",
+                    level: "UBERS",
+                    status: "ENROLLMENT"
+                },
+                {
+                    tournamentId: "507c7f79bcf86cd7994f6c1e",
+                    name: "Tournament 2",
+                    beginDate: "2016-05-20 21:00",
+                    level: "OVERLY USED",
+                    status: "ENROLLMENT"
+                },
+                {
+                    tournamentId: "507c7f79bcf86cd7994f6c2e",
+                    name: "Tournament 3",
+                    beginDate: "2016-10-29 18:00",
+                    level: "ANYTHING GOES",
+                    status: "ENROLLMENT"
+                },
+                {
+                    tournamentId: "507c7f79bcf86cd7994f6c3e",
+                    name: "Tournament 4",
+                    beginDate: "2016-02-02 12:00",
+                    level: "OVERLY USED",
+                    status: "ENROLLMENT"
+                },
+                {
+                    tournamentId: "507c7f79bcf86cd7994f6c4e",
+                    name: "Tournament 5",
+                    beginDate: "2016-10-29 18:00",
+                    level: "UBERS",
+                    status: "ENROLLMENT"
+                },
+                {
+                    tournamentId: "507c7f79bcf86cd7994f6c5e",
+                    name: "Tournament 6",
+                    beginDate: "2016-10-09 09:00",
+                    level: "NEVER USED",
+                    status: "ENROLLMENT"
+                },
+                {
+                    tournamentId: "507c7f79bcf86cd7994f6c6e",
+                    name: "Tournament 7",
+                    beginDate: "2016-11-19 18:00",
+                    level: "UBERS",
+                    status: "ENROLLMENT"
+                },
+            ];
             tournaments.refereeTournaments = [];
-            tournaments.participantTournaments = ['Tournament 1', 'Tournament2', 'Tournament3'];
+            tournaments.participantTournaments = [
+                {
+                    tournamentId: "507c7f79bcf86cd7994f6c7e",
+                    name: "Tournament 11",
+                    beginDate: "2016-12-19 19:00",
+                    level: "UBERS",
+                    status: "ENROLLMENT"
+                },
+                {
+                    tournamentId: "507c7f79bcf86cd7994f6c8e",
+                    name: "Tournament 12",
+                    beginDate: "2016-05-20 21:00",
+                    level: "OVERLY USED",
+                    status: "ENROLLMENT"
+                },
+                {
+                    tournamentId: "507c7f79bcf86cd7994f6c9e",
+                    name: "Tournament 13",
+                    beginDate: "2016-10-29 18:00",
+                    level: "ANYTHING GOES",
+                    status: "ENROLLMENT"
+                }];
             return tournaments;
         }
 
         return service;
 
     }
-    
+
 
 })();
