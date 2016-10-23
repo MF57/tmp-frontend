82bf48effd65e2aac98c36a1fcf15df1638f3cdb
MF57 <p-bochenek@outlook.com>
2016-10-23 15:17:19 +0200

    enrollme no tournaments view

--- app/partials/auth/enrollMe/enrollMe.service.js
+++ app/partials/auth/enrollMe/enrollMe.service.js
@@ -17,20 +17,20 @@
 
         function loadAll(username) {
             return [
-                {
-                    tournamentId: "507c7f79bcf86cd7994f6c0e",
-                    name: "Tournament 1",
-                    beginDate: "2016-12-19 19:00",
-                    level: "UBERS",
-                    status: "ENROLLMENT"
-                },
-                {
-                    tournamentId: "507c7f79bcf86cd7994f6c1e",
-                    name: "Tournament 2",
-                    beginDate: "2016-05-20 21:00",
-                    level: "OVERLY USED",
-                    status: "ENROLLMENT"
-                }
+                // {
+                //     tournamentId: "507c7f79bcf86cd7994f6c0e",
+                //     name: "Tournament 1",
+                //     beginDate: "2016-12-19 19:00",
+                //     level: "UBERS",
+                //     status: "ENROLLMENT"
+                // },
+                // {
+                //     tournamentId: "507c7f79bcf86cd7994f6c1e",
+                //     name: "Tournament 2",
+                //     beginDate: "2016-05-20 21:00",
+                //     level: "OVERLY USED",
+                //     status: "ENROLLMENT"
+                // }
             ];
         }
 
